e525dae3f278a97a0b6df1c2e6cb1487bb7107ed
BoonDoPark <BoonDoPark>
2024-06-24 21:41:24 +0900

    [FIX] 데이터 타입 수정

--- schedulers/lotto_num_scheduler.py
+++ schedulers/lotto_num_scheduler.py
@@ -1,12 +1,17 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.generate_lotto_num import lotto_result
-import requests, json
+import requests
 
 scheduler = BackgroundScheduler(timezone='Asia/Seoul')
 analytics_url = 'http://localhost:8000/api/v1/lotto'
+lotto_result_url = 'http://localhost:8080/api/v1/lotto/result'
+headers = {'Content-Type': 'application/json'}
 
 def lotto_num_result_job():
     print(lotto_result())
-    requests.post(analytics_url, json=lotto_result())
-# minutes=1
-scheduler.add_job(lotto_num_result_job, 'interval', seconds=15)
+    data = lotto_result()
+    requests.post(analytics_url, json=data, headers=headers)
+    requests.post(lotto_result_url, json=data, headers=headers)
+
+
+scheduler.add_job(lotto_num_result_job, 'interval', seconds=30)
